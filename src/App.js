9d4f9803a5488c5ad5c5a73aa538f860a51fedd6
Diyor11 <77630836+Diyor11@users.noreply.github.com>
2021-09-14 13:00:11 +0500

    import map from component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Discover from './component/Dicover/Discover';
 import Info from './component/Info/Info';
 import FooterInfo from './component/FootInfo/FooterInfo';
 import Footer from './component/Footer/Footer';
+import YandexMap from './component/Map/YandexMap';
 
 const App = () => {
     return (
@@ -18,6 +19,7 @@ const App = () => {
            <Discover />
            <Info />
            <FooterInfo />
+           <YandexMap />
            <Footer />
         </>
     );
